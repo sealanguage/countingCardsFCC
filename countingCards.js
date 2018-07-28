32ed75fbc44e94b55d96b1b8c9b5fa072655a9a8
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-07-28 17:04:03 +0000

    solved

--- countingCards.js
+++ countingCards.js
@@ -1,36 +1,38 @@
-// counting cards not working yet
-
+// counting cards solved
 var count = 0;
 
 function cc(card) {
   // Only change code below this line
   
-for(var i = 0; i < cardseq; i++) {
-   
-   if (card >= 2 && card <= 6) {
-    count++;
-    console.log("high" + count);
-  } else if (card === 10) {
-    count--;
-    // console.log("mid" + count);
-  } else if (card == 'J', 'Q', 'K', 'A') {
-    count--;
-    // console.log("low" + count);
+  switch(card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count ++;
+    break; 
+
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+    count --;
+    break;
   }
-}
 
-if(count > 0) {
-  return count + "Bet";
-} else if (count <= 0) {
-  return count + "Hold";
-  return count;
-}
+  if(count >= 1) {
+      return count + " Bet";
+      console.log("count is ", count, "card is ", card);
+    } else if (count <= 0) {
+      return count + " Hold";
 
- 
-  
-  // return "count";
-  // Only change code above this line
-}
+      console.log("count is ", count, "card is ", card);
+    }
+  }
+
+
 
 // Add/remove calls to test your function.
 // Note: Only the last will display
